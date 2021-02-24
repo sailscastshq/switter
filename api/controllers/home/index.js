b6ad953ac6598e08666bf8edeab8a358cd791a29
Kelvin Omereshone <kelvinomereshone@gmail.com>
2021-02-24 21:37:38 +0100

    feat: initial commit

---
+++ api/controllers/home/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Index',
+
+
+  description: 'Index home.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
